09a113847a0ebd9cdd662541eb10440266045664
abiakarn <nora.abiakar@gmail.com>
2019-07-16 15:00:29 +0200

    more unit tests

--- test/unit/inputs/gen_nodes.py
+++ test/unit/inputs/gen_nodes.py
@@ -3,12 +3,15 @@ import numpy as np
 
 f0 = h5py.File("nodes_0.h5", "a")
 f1 = h5py.File("nodes_1.h5", "a")
+f2 = h5py.File("nodes_2.h5", "a")
 
 nodes0 = f0.create_group('nodes')
 nodes1 = f1.create_group('nodes')
+nodes2 = f2.create_group('nodes')
 
 pop_e = nodes0.create_group('pop_e')
 pop_i = nodes1.create_group('pop_i')
+pop_ext = nodes2.create_group('pop_ext')
 
 node_group_id = pop_e.create_dataset("node_group_id", (4,), dtype='i')
 for i in range(0,4):
@@ -31,8 +34,8 @@ gl_hh = dyn_param.create_dataset("hh_0.gl_hh", (4,), dtype="f")
 el_hh = dyn_param.create_dataset("hh_0.el_hh", (4,), dtype="f")
 
 for i in range(0,4):
-    g_pas[i] = .001
-    e_pas[i] = -65
+    g_pas[i] = 0.001
+    e_pas[i] = -65.1
     gl_hh[i] = 0.0003
     el_hh[i] = -54.3
 
@@ -53,20 +56,28 @@ for i in range(0,1):
 g0 = pop_i.create_group("0")
 dyn_param = g0.create_group("dynamics_params")
 
-g_pas = dyn_param.create_dataset("pas_0.g_pas", (1,), dtype="f")
-e_pas = dyn_param.create_dataset("pas_0.e_pas", (1,), dtype="f")
-gl_hh = dyn_param.create_dataset("hh_0.gl_hh", (1,), dtype="f")
-el_hh = dyn_param.create_dataset("hh_0.el_hh", (1,), dtype="f")
-
 dt = h5py.special_dtype(vlen=bytes)
 morph = g0.create_dataset("morphology", (1,), dtype='S100')
 
 for i in range(0,1):
-    g_pas[i] = .001
-    e_pas[i] = -65
-    gl_hh[i] = 0.0003
-    el_hh[i] = -54.3
     morph[i] = np.string_("/home/abiakarn/git/arbor-sonata/test/unit/inputs/soma.swc")
+
+############################################################################
+
+node_group_id = pop_ext.create_dataset("node_group_id", (1,), dtype='i')
+for i in range(0,1):
+    node_group_id[i] = 0
+
+node_group_index = pop_ext.create_dataset("node_group_index", (1,), dtype='i')
+for i in range(0,1):
+    node_group_index[i] = i
+
+node_type_id = pop_ext.create_dataset("node_type_id", (1,), dtype='i')
+for i in range(0,1):
+    node_type_id[i] = 200
+
+g0 = pop_ext.create_group("0")
 
 f0.close()
 f1.close()
+f2.close()
--- test/unit/test_hdf5.cpp
+++ test/unit/test_hdf5.cpp
@@ -5,44 +5,51 @@
 
 #include "../gtest.h"
 
-//                  ________________
-//                 |                v
+// pop_ext         n5
+//           ______|_______
+//          |       _______|________
+//          |      |       |        |
+//          v      |       v        v
 //  pop_e:  n0     n1      n2       n3
-//           |     ^       |
-//           |     |       |
-//           |     |       |
-//  pop_i:   |___> n5 <____|
-
+//          |      ^       |
+//          |      |       |
+//          |      |       |
+//  pop_i:  |____> n4 <____|
 
 TEST(hdf5_record, verify_nodes) {
     std::string datadir{DATADIR};
 
     auto filename0 = datadir + "/nodes_0.h5";
     auto filename1 = datadir + "/nodes_1.h5";
+    auto filename2 = datadir + "/nodes_2.h5";
 
     auto f0 = std::make_shared<h5_file>(filename0);
     auto f1 = std::make_shared<h5_file>(filename1);
+    auto f2 = std::make_shared<h5_file>(filename2);
 
-    h5_record r({f0, f1});
+    h5_record r({f0, f1, f2});
 
     EXPECT_TRUE(r.verify_nodes());
     EXPECT_THROW(r.verify_edges(), sonata_exception);
 
-    EXPECT_EQ(std::vector<unsigned>({0u, 4u, 5u}), r.partitions());
-    EXPECT_EQ(5, r.num_elements());
+    EXPECT_EQ(std::vector<unsigned>({0u, 4u, 5u, 6u}), r.partitions());
+    EXPECT_EQ(6, r.num_elements());
 
     auto pops = r.populations();
     auto map = r.map();
 
-    EXPECT_EQ(pops.size(), 2);
+    EXPECT_EQ(pops.size(), 3);
     EXPECT_EQ("pop_e", pops[0].name());
     EXPECT_EQ("pop_i", pops[1].name());
+    EXPECT_EQ("pop_ext", pops[2].name());
 
     EXPECT_EQ(0, map["pop_e"]);
     EXPECT_EQ(1, map["pop_i"]);
+    EXPECT_EQ(2, map["pop_ext"]);
 
     EXPECT_EQ(r[0].name(), r["pop_e"].name());
     EXPECT_EQ(r[1].name(), r["pop_i"].name());
+    EXPECT_EQ(r[2].name(), r["pop_ext"].name());
 
     for (unsigned i = 0; i < 4; i++) {
         auto l = r.localize(i);
@@ -53,10 +60,15 @@ TEST(hdf5_record, verify_nodes) {
     EXPECT_EQ("pop_i", l.pop_name);
     EXPECT_EQ(0, l.el_id);
 
+    l = r.localize(5);
+    EXPECT_EQ("pop_ext", l.pop_name);
+    EXPECT_EQ(0, l.el_id);
+
     for (unsigned i = 0; i < 4; i++) {
         EXPECT_EQ(i, r.globalize({"pop_e", i}));
     }
     EXPECT_EQ(4, r.globalize({"pop_i", 0}));
+    EXPECT_EQ(5, r.globalize({"pop_ext", 0}));
 }
 
 TEST(hdf5_record, verify_edges) {
@@ -65,34 +77,39 @@ TEST(hdf5_record, verify_edges) {
     auto filename0 = datadir + "/edges_0.h5";
     auto filename1 = datadir + "/edges_1.h5";
     auto filename2 = datadir + "/edges_2.h5";
+    auto filename3 = datadir + "/edges_3.h5";
 
     auto f0 = std::make_shared<h5_file>(filename0);
     auto f1 = std::make_shared<h5_file>(filename1);
     auto f2 = std::make_shared<h5_file>(filename2);
+    auto f3 = std::make_shared<h5_file>(filename3);
 
-    h5_record r({f0, f1, f2});
+    h5_record r({f0, f1, f2, f3});
 
     EXPECT_TRUE(r.verify_edges());
     EXPECT_THROW(r.verify_nodes(), sonata_exception);
 
-    EXPECT_EQ(std::vector<unsigned>({0u, 2u, 3u, 4u}), r.partitions());
-    EXPECT_EQ(4, r.num_elements());
+    EXPECT_EQ(std::vector<unsigned>({0u, 2u, 3u, 4u, 6u}), r.partitions());
+    EXPECT_EQ(6, r.num_elements());
 
     auto pops = r.populations();
     auto map = r.map();
 
-    EXPECT_EQ(pops.size(), 3);
+    EXPECT_EQ(pops.size(), 4);
     EXPECT_EQ("pop_e_i", pops[0].name());
     EXPECT_EQ("pop_i_e", pops[1].name());
     EXPECT_EQ("pop_e_e", pops[2].name());
+    EXPECT_EQ("pop_ext_e", pops[3].name());
 
     EXPECT_EQ(0, map["pop_e_i"]);
     EXPECT_EQ(1, map["pop_i_e"]);
     EXPECT_EQ(2, map["pop_e_e"]);
+    EXPECT_EQ(3, map["pop_ext_e"]);
 
     EXPECT_EQ(r[0].name(), r["pop_e_i"].name());
     EXPECT_EQ(r[1].name(), r["pop_i_e"].name());
     EXPECT_EQ(r[2].name(), r["pop_e_e"].name());
+    EXPECT_EQ(r[3].name(), r["pop_ext_e"].name());
 
     EXPECT_EQ(2, r["pop_e_i"].dataset_size("edge_group_id"));
     EXPECT_EQ(0, r["pop_e_i"].int_at("edge_group_id",0));
