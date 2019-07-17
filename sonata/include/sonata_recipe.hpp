e5e166a775368cd1336afb8b73a8d1b1145e904c
abiakarn <nora.abiakar@gmail.com>
2019-07-17 13:56:28 +0200

    refactor database into model_desc and io_desc; move probe quesrying into io_desc

--- sonata/include/sonata_recipe.hpp
+++ sonata/include/sonata_recipe.hpp
@@ -40,16 +40,18 @@ using arb::cell_probe_address;
 class sonata_recipe: public arb::recipe {
 public:
     sonata_recipe(sonata_params params):
-            database_(params.network.nodes,
-                      params.network.edges,
-                      params.network.nodes_types,
-                      params.network.edges_types,
-                      params.spikes_input,
-                      params.current_clamps),
+            model_desc_(params.network.nodes,
+                       params.network.edges,
+                       params.network.nodes_types,
+                       params.network.edges_types),
+            io_desc_(params.network.nodes,
+                        params.spikes_input,
+                        params.current_clamps,
+                        params.probes_info),
             run_params_(params.run),
             sim_cond_(params.conditions),
             probe_info_(params.probes_info),
-            num_cells_(database_.num_cells()) {}
+            num_cells_(model_desc_.num_cells()) {}
 
     cell_size_type num_cells() const override {
         return num_cells_;
@@ -57,7 +59,7 @@ public:
 
     void build_local_maps(const arb::domain_decomposition& decomp) {
         std::lock_guard<std::mutex> l(mtx_);
-        database_.build_source_and_target_maps(decomp.groups);
+        model_desc_.build_source_and_target_maps(decomp.groups);
     }
 
     arb::util::unique_any get_cell_description(cell_gid_type gid) const override {
@@ -66,10 +68,10 @@ public:
             std::vector<std::pair<arb::segment_location, arb::mechanism_desc>> tgt_types;
 
             std::lock_guard<std::mutex> l(mtx_);
-            auto morph = database_.get_cell_morphology(gid);
-            auto mechs = database_.get_density_mechs(gid);
+            auto morph = model_desc_.get_cell_morphology(gid);
+            auto mechs = model_desc_.get_density_mechs(gid);
 
-            database_.get_sources_and_targets(gid, src_locs, tgt_types);
+            model_desc_.get_sources_and_targets(gid, src_locs, tgt_types);
 
             std::vector<std::pair<arb::segment_location, double>> src_types;
             for (auto s: src_locs) {
@@ -78,7 +80,7 @@ public:
 
             auto cell = dummy_cell(morph, mechs, src_types, tgt_types);
 
-            auto stims = database_.get_current_clamps(gid);
+            auto stims = io_desc_.get_current_clamps(gid);
             for (auto s: stims) {
                 arb::i_clamp stim(s.delay, s.duration, s.amplitude);
                 cell.add_stimulus(s.stim_loc, stim);
@@ -88,31 +90,31 @@ public:
         }
         else if (get_cell_kind(gid) == cell_kind::spike_source) {
             std::lock_guard<std::mutex> l(mtx_);
-            std::vector<double> time_sequence = database_.get_spikes(gid);
+            std::vector<double> time_sequence = io_desc_.get_spikes(gid);
             return arb::util::unique_any(arb::spike_source_cell{arb::explicit_schedule(time_sequence)});
         }
     }
 
     cell_kind get_cell_kind(cell_gid_type gid) const override {
         std::lock_guard<std::mutex> l(mtx_);
-        return database_.get_cell_kind(gid);
+        return model_desc_.get_cell_kind(gid);
     }
 
     cell_size_type num_sources(cell_gid_type gid) const override {
         std::lock_guard<std::mutex> l(mtx_);
-        return database_.num_sources(gid);
+        return model_desc_.num_sources(gid);
     }
 
     cell_size_type num_targets(cell_gid_type gid) const override {
         std::lock_guard<std::mutex> l(mtx_);
-        return database_.num_targets(gid);
+        return model_desc_.num_targets(gid);
     }
 
     std::vector<arb::cell_connection> connections_on(cell_gid_type gid) const override {
         std::vector<arb::cell_connection> conns;
 
         std::lock_guard<std::mutex> l(mtx_);
-        database_.get_connections(gid, conns);
+        model_desc_.get_connections(gid, conns);
 
         return conns;
     }
@@ -123,51 +125,15 @@ public:
     }
 
     cell_size_type num_probes(cell_gid_type gid)  const override {
-        std::lock_guard<std::mutex> l(mtx_);
-        std::string population = database_.population_of(gid);
-
-        unsigned sum = 0;
-        for (auto p: probe_info_) {
-            if (population == p.population) {
-                if (p.node_ids.empty()) {
-                    sum++;
-                } else {
-                    if (std::binary_search(p.node_ids.begin(), p.node_ids.end(), database_.population_id_of(gid))) {
-                        sum++;
-                    }
-                }
-            }
-        }
-        return sum;
+        return io_desc_.get_num_probes(gid);
     }
 
     arb::probe_info get_probe(cell_member_type id) const override {
-        std::lock_guard<std::mutex> l(mtx_);
-        std::string population = database_.population_of(id.gid);
+        return io_desc_.get_probe(id);
+    }
 
-        unsigned loc = 0;
-        for (auto p: probe_info_) {
-            if (population == p.population) {
-                if (p.node_ids.empty() || std::binary_search(p.node_ids.begin(), p.node_ids.end(), database_.population_id_of(id.gid))) {
-                    if (loc == id.index) {
-                        // Get the appropriate kind for measuring voltage.
-                        cell_probe_address::probe_kind kind;
-                        if (p.kind == "v") {
-                            kind = cell_probe_address::membrane_voltage;
-                        } else if (p.kind == "i") {
-                            kind = cell_probe_address::membrane_current;
-                        } else {
-                            throw sonata_exception("Probe kind not supported");
-                        }
-                        arb::segment_location loc(p.sec_id, p.sec_pos);
-
-                        probe_files_[id] = p.file_name;
-                        return arb::probe_info{id, kind, cell_probe_address{loc, kind}};
-                    }
-                    loc++;
-                }
-            }
-        }
+    std::unordered_map<std::string, std::vector<cell_member_type>> get_probe_groups() {
+        return io_desc_.get_probe_groups();
     }
 
     arb::util::any get_global_properties(cell_kind k) const override {
@@ -177,26 +143,22 @@ public:
         return a;
     }
 
-    std::string get_probe_file(cell_member_type id) const {
-        return probe_files_[id];
-    }
-
     std::vector<unsigned> get_pop_partitions() const {
-        return database_.pop_partitions();
+        return model_desc_.pop_partitions();
     }
 
     std::vector<std::string> get_pop_names() const {
-        return database_.pop_names();
+        return model_desc_.pop_names();
     }
 
 private:
     mutable std::mutex mtx_;
-    mutable database database_;
+    mutable model_desc model_desc_;
+    mutable io_desc io_desc_;
 
     run_params run_params_;
     sim_conditions sim_cond_;
     std::vector<probe_info> probe_info_;
 
-    mutable std::unordered_map<cell_member_type, std::string> probe_files_;
     cell_size_type num_cells_;
 };
